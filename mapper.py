fac37e53f888750f5856d667f526ec6af15420c6
AmalRozario <amal.rozario@gmail.com>
2024-01-29 03:58:38 +0000

    added more stopwords and puntuation

--- mapper.py
+++ mapper.py
@@ -6,9 +6,15 @@ for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
 
+    # make lower case
+    line = line.lower()
+
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
+    stopwordsandpunct = set(['the', 'and','i','to','of','you','a',"'",'.',';',',',':','!','?'])
+
     for word in words:
-        print '%s\t%s' % (word, "1")
+	if word not in stopwordsandpunct:
+           print '%s\t%s' % (word, "1")
